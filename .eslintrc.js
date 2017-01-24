7e89e2b547e8c841dd806efeddc8fa28d6e2cbf4
Geoff Whatley <me@geoffwhatley.com>
2017-01-25 10:47:51 +1100

    refactor(lint): ESLint config moved to JS file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  rules: {
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': [ 2, { 'ignore': [ '^[~]' ] }],
+    'react/forbid-prop-types': 0,
+    'react/jsx-filename-extension': 0,
+    'new-cap': [2, { 'capIsNewExceptions': ['List', 'Map'] }]
+  }
+}
