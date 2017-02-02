bf7190292ceab34991660fe3855641b3617bde15
Geoff Whatley <me@geoffwhatley.com>
2017-02-03 10:52:40 +1100

    feat(ui): Integrate shoutem-ui for native development

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,12 @@ module.exports = {
   rules: {
     'import/extensions': 0,
     'import/no-extraneous-dependencies': 0,
+    'import/no-dynamic-require': 0,
     'import/no-unresolved': [2, { 'ignore': [ '^[~]', '^[#]'] }],
     'import/prefer-default-export': 1,
     'react/forbid-prop-types': 0,
     'react/jsx-filename-extension': 0,
+    'global-require': 0,
     'new-cap': [2, { 'capIsNewExceptions': ['List', 'Map'] }],
     'no-unused-vars': [2, { args: 'none' }],
     'no-underscore-dangle': [2, { allow: ['_id'] }],
--- src/native/index.js
+++ src/native/index.js
@@ -1,44 +1,51 @@
-import Exponent from 'exponent';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import Exponent, { Components, Font } from 'exponent';
+import { Title, Subtitle, Text, View } from '@shoutem/ui';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  title: {
-    fontSize: 40,
-    margin: 10,
-    textAlign: 'center',
-  },
-  information: {
-    fontSize: 20,
-    color: '#333333',
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-});
+const { AppLoading } = Components;
+
+console.disableYellowBox = true;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    fontsLoaded: false,
+  }
 
-    this.state = { title: 'strap' };
+  async componentWillMount() {
+    await Font.loadAsync({
+      'Rubik-Black': require('../../assets/fonts/Rubik-Black.ttf'),
+      'Rubik-BlackItalic': require('../../assets/fonts/Rubik-BlackItalic.ttf'),
+      'Rubik-Bold': require('../../assets/fonts/Rubik-Bold.ttf'),
+      'Rubik-BoldItalic': require('../../assets/fonts/Rubik-BoldItalic.ttf'),
+      'Rubik-Italic': require('../../assets/fonts/Rubik-Italic.ttf'),
+      'Rubik-Light': require('../../assets/fonts/Rubik-Light.ttf'),
+      'Rubik-LightItalic': require('../../assets/fonts/Rubik-LightItalic.ttf'),
+      'Rubik-Medium': require('../../assets/fonts/Rubik-Medium.ttf'),
+      'Rubik-MediumItalic': require('../../assets/fonts/Rubik-MediumItalic.ttf'),
+      'Rubik-Regular': require('../../assets/fonts/Rubik-Regular.ttf'),
+      'rubicon-icon-font': require('../../assets/fonts/rubicon-icon-font.ttf'),
+    });
+
+    this.setState({ fontsLoaded: true });
   }
 
   render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          {this.state.title}
+      <View styleName="flexible md-gutter vertical v-center h-center">
+        <Title styleName="sm-gutter-bottom">strap</Title>
+        <Subtitle styleName="md-gutter-bottom">
+          Universal Javascript MVP Boilerplate
+        </Subtitle>
+        <Text styleName="h-center">
+          A universal boilerplate project for Javascript flavored MVP style development,
+          with a focus on integrating modern tooling for maximum productivity.
         </Text>
-        <Text style={styles.information}>
-          A boilerplate project for MVP style development, with a focus on integrating
-        modern tooling for maximum productivity.
-        </Text>
+        <StatusBar barStyle="default" hidden={false} />
       </View>
     );
   }
