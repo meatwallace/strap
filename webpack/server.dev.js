882403a0d634b5e3d0c6dfe9fe655a45f56289ac
Geoff Whatley <me@geoffwhatley.com>
2017-03-15 15:25:17 +1100

    build(webpack): Add Bundle Analyzer plugin

--- webpack/server.dev.js
+++
@@ -1,9 +0,0 @@
-import merge from 'webpack-merge';
-import base from './server.base';
-
-module.exports = function config() {
-  const baseConfig = base();
-  const devConfig = {};
-
-  return merge(baseConfig, devConfig);
-};
