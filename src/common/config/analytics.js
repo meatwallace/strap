0b9c8836e4d756cebfed9b64fa387e960093b820
Geoff Whatley <me@geoffwhatley.com>
2017-03-20 13:34:11 +1100

    feat(analytics): Create Analytics middleware and action creator

--- src/common/config/analytics.js
+++ src/common/config/analytics.js
@@ -1,3 +1,14 @@
 import Analytics from 'analytics-react-native';
 
-export default new Analytics(process.env.SEGMENT_KEY);
+const analytics = new Analytics(process.env.SEGMENT_KEY);
+
+export const EVENT_TYPES = {
+  IDENTIFY: analytics.identify,
+  TRACK: analytics.track,
+  SCREEN: analytics.screen,
+  PAGE: analytics.page,
+  GROUP: analytics.group,
+  ALIAS: analytics.alias,
+}
+
+export default analytics;
--- src/native/middleware/index.js
+++ src/native/middleware/index.js
@@ -1,7 +1,9 @@
+import analytics from '@common/middleware';
 import client from '../config/apollo';
 
 const middleware = [
   client.middleware(),
+  analytics,
 ];
 
 export default middleware;
